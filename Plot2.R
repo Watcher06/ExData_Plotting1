7d2b11635b4112c161cb911cefe21df61f698fa7
Watcher06 <rajani.ekta@gmail.com>
2018-03-12 10:41:20 +0400

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,8 +1,9 @@
 
 ##first few lines of code is same as the Plot1.R file
-
-
-
+file1<-read.table("./EP/household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors=FALSE,dec=".")
+subsetfile1<-file1[file1$Date %in% c("1/2/2007","2/2/2007"),]
+str(subsetfile1)
+global_active_power<-as.numeric(subsetfile1$Global_active_power)
 
 
 datetime<-strptime(paste(subsetfile1$Date,subsetfile1$Time,sep=""),"%d/%m/%Y %H:%M:%S")
